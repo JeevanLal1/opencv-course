2d5efd05a084eca5772b7da7abacf32164be9361
JeevanLal1 <150320695+JeevanLal1@users.noreply.github.com>
2024-09-06 00:05:03 +0530

    Update blurring.py

--- Section #2 - Advanced/blurring.py
+++ Section #2 - Advanced/blurring.py
@@ -6,19 +6,21 @@ img = cv.imread('../Resources/Photos/cats.jpg')
 cv.imshow('Cats', img)
 
 # Averaging
+#here kernel size is (3,3) ,kernel means a window/matrix where all values are taken except the center 
+#one and calculate the average of put int hte center and then slide it for later image 
 average = cv.blur(img, (3,3))
 cv.imshow('Average Blur', average)
 
-# Gaussian Blur
+# Gaussian Blur(more effective for blurring )
 gauss = cv.GaussianBlur(img, (3,3), 0)
 cv.imshow('Gaussian Blur', gauss)
 
-# Median Blur
+# Median Blur(more effective than gaussian blur and used to remove noise)
 median = cv.medianBlur(img, 3)
 cv.imshow('Median Blur', median)
 
-# Bilateral
+# Bilateral(Most Effective)
 bilateral = cv.bilateralFilter(img, 10, 35, 25)
 cv.imshow('Bilateral', bilateral)
-
+#try to keep lower value for medianblur and bilateral
 cv.waitKey(0)
