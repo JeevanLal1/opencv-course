2da0863ee6181c5ca05188adb298b100ae292b6e
JeevanLal1 <150320695+JeevanLal1@users.noreply.github.com>
2024-09-06 00:10:18 +0530

    Update bitwise.py

--- Section #2 - Advanced/bitwise.py
+++ Section #2 - Advanced/bitwise.py
@@ -23,7 +23,7 @@ cv.imshow('Bitwise OR', bitwise_or)
 bitwise_xor = cv.bitwise_xor(rectangle, circle)
 cv.imshow('Bitwise XOR', bitwise_xor)
 
-# bitwise NOT
+# bitwise NOT-->inversion
 bitwise_not = cv.bitwise_not(circle)
 cv.imshow('Circle NOT', bitwise_not)
 
